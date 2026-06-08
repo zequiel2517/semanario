b6fd730b773aa9bf606cbac9aeb79f62c1960771
zequiel2517 <zequiel2517@gmail.com>
2026-06-08 14:05:02 +0200

    Add files via upload

--- data.js
+++ data.js
@@ -23,6 +23,7 @@ const DATOS_INICIALES = {
         { id: "pelayo",   nombre: "Pelayo",   color: "#B5EAD7" }, // verde menta
         { id: "cristina", nombre: "Cristina", color: "#FFB6C1" }, // rosa pastel
         { id: "hector",   nombre: "Héctor",   color: "#FFEAA7" }  // amarillo crema
+        { id: "familia",  nombre: "Familia",  color: "#B8C6D9" }  // gris azulado
     ],
 
     eventosRecurrentes: {
